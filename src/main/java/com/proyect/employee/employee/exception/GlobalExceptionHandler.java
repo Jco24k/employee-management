e3556400903b9cdae613e3509c29e0f4a6b606ba
Jco <jesus14k02@outlook.es>
2023-10-17 11:43:24 -0500

    updated RestControllerAdvice

--- src/main/java/com/proyect/employee/employee/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proyect/employee/employee/exception/GlobalExceptionHandler.java
@@ -1,21 +1,21 @@
 package com.proyect.employee.employee.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDetails handleResourceNotFoundException(
             ResourceNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
     }
 }
